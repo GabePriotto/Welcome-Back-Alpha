a448de3a57924dabde10ebd04c6c4bd9b8a7ed5d
GabePriotto <162745202+GabePriotto@users.noreply.github.com>
2024-12-06 18:39:42 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,9 @@
+window.alert('Boas-vindas, amigo!');
+
+
+
+var nome = prompt("Antes de mais nada, poderia nos dizer o seu nome?");
+nome === ""? alert("Tudo bem! Não precisa responder se não quiser!"): alert("Seja muito bem-vindo, " + nome + "!");
+alert("Infelizmente, o site está passando por uma reconstrução do código fonte devido a erros constantes. O site esquecerá seu nome e sua experiência pode ser de nível inferior...");
+alert("Faremos o possível para que você possa realizar ações simples até o site ser reformado =)");
+alert("Vejo você mais tarde, " + nome + " =)");
